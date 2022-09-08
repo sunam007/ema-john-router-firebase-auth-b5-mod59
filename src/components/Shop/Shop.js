be1329e87bb7923c321071495274a910ca95a899
Asaduzzaman Sunam <asaduzzaman.sunam@gmail.com>
2022-09-09 01:34:00 +0600

    grid layout,load data, use state

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Shop.css";
 
 const Shop = () => {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    fetch("./products.json")
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, []);
+
   return (
     <div className="shop-container">
       <div className="product-container">
-        <h3>Products:</h3>
+        <h3>Products: {products.length} </h3>
+        {products.map((product) => console.log(product))}
       </div>
       <div className="cart-container">
         <h3>Order Summary:</h3>
