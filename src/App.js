ae8ccd70255a0046d2b192817c9941fafc74a7bd
Asaduzzaman Sunam <asaduzzaman.sunam@gmail.com>
2022-09-09 00:47:58 +0600

    shop container added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Header from "./components/Header/Header";
+import Shop from "./components/Shop/Shop";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Header></Header>
+      <Shop></Shop>
+    </div>
+  );
 }
 
 export default App;
